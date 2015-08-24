c22c46d090e05af618d1282c007b609f6dfd40d4
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-24 15:57:28 +0200

    Made views, public and lib folder, created ruby code to print random phrase on refresh

---
+++ phrases_app/lib/iterate.rb
@@ -0,0 +1,8 @@
+class Iterate
+	def initialize(array)
+		@array = array
+	end
+	def new_iterate
+	   @array.sample
+    end
+end
--- phrases_app/phrases_app.rb
+++ phrases_app/phrases_app.rb
@@ -1,6 +1,19 @@
 require "sinatra"
 require "sinatra/reloader"
+require_relative "./lib/iterate.rb"
 
-get "/" do 
-	"Hello, this is the first test!"
+
+
+get "/index" do 
+	phrases = Iterate.new(["I like beer.", "I play tennis.", "Travelling is my favourite activity."])
+	@phrase = phrases.new_iterate
+	
+	erb :index
+end
+
+
+post "/input" do
+
+	erb :index
+
 end
