4bc431376470b69adee54ae9c3bfac784fc4ca0f
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-24 22:29:47 +0200

    Added substraction, multiplication and division to the calculator

--- online_calculator/calculator.rb
+++ online_calculator/calculator.rb
@@ -8,11 +8,39 @@ get "/" do
 	erb :index
 end
 
-post "/input" do
+post "/addition" do
 	addition1 = params[:addition1]
 	addition2 = params[:addition2]
 	new_calculation = Calculator.new(addition1,addition2)
-	
 	@adding_result = new_calculation.addition
+	erb :index
+end
+
+
+post "/substraction" do
+	substraction1 = params[:substraction1]
+	substraction2 = params[:substraction2]
+	new_calculation = Calculator.new(substraction1,substraction2)
+	@substraction_result = new_calculation.substraction
+	erb :index
+end
+
+
+
+post "/multiplication" do
+	multiplication1 = params[:multiplication1]
+	multiplication2 = params[:multiplication2]
+	new_calculation = Calculator.new(multiplication1,multiplication2)
+	@multiplication_result = new_calculation.multiplication
+	erb :index
+end
+
+
+
+post "/division" do
+	division1 = params[:division1]
+	division2 = params[:division2]
+	new_calculation = Calculator.new(division1,division2)
+	@division_result = new_calculation.division
 	erb :index
 end
--- online_calculator/lib/operations.rb
+++ online_calculator/lib/operations.rb
@@ -1,9 +1,18 @@
 class Calculator
 	def initialize(number1,number2)
-		@number1 = number1
-		@number2 = number2
+		@number1 = number1.to_i
+		@number2 = number2.to_i
 	end
 	def addition
-		@number1.to_i + @number2.to_i
+		@number1 + @number2
+	end
+	def substraction
+		@number1 - @number2
+	end
+	def multiplication
+		@number1 * @number2
+	end
+	def division
+		@number1 / @number2
 	end
 end
