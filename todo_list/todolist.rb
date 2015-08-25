012690545cb2de89f07a3b81997f9f30a89cd80a
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-25 16:52:48 +0200

    Made another list for done tasks, added basic styling

--- todo_list/todolist.rb
+++ todo_list/todolist.rb
@@ -1,12 +1,15 @@
 require "sinatra"
 require "sinatra/reloader"
 require "pry"
+require_relative "./lib/storage.rb"
 
 
 list = []
+completed = []
 
 get "/" do 
 	@list = list
+	@completed = completed
 	erb :index
 end
 
@@ -18,7 +21,9 @@ end
 
 
 post "/remove_task" do 
-	task = params[:task_to_delete].to_i
-	list.delete_at(task)
+	index_task = params[:task_to_delete].to_i
+	done_task = list[index_task]
+	list.delete_at(index_task)
+	completed.push(done_task)	
 	redirect to("/")
 end
