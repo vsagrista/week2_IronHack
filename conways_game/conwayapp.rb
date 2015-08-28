1fbde678e8011ed3ae25caed22f67e59d18cd17c
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-28 13:05:29 +0200

    Created class 'Cell' to create a new cell and send it to class 'Neighbor' for analysing its relatives

--- conways_game/conwayapp.rb
+++ conways_game/conwayapp.rb
@@ -1,9 +1,11 @@
 require_relative "./grid.rb"
+
 class Cell
   def send_to_neighbors(y,x)
   	Neighbors.new.check_neighbors(y,x)
   end 
 end
+
 
 new_cell = Cell.new
 new_cell.send_to_neighbors(0,0)
