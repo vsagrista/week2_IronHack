e85b0cd8a41b62eb298f762ba02695c741e5823d
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-28 17:15:28 +0200

    Made get and post methods for the app

--- movie_lovers/movieloversapp.rb
+++ movie_lovers/movieloversapp.rb
@@ -1,4 +1,16 @@
-require "imdb"
 
-i = Imdb::Search.new("Star Trek")
-p i.title
+require "sinatra"
+require "sinatra/reloader"
+require "pry"
+require_relative "./lib/logic.rb"
+
+
+get "/" do 
+	erb :index
+end
+
+post "/search_keyword" do 
+	keyword = params[:keyword]
+	@posters = Movie.new.get_movies_by_keyword(keyword)
+	erb :index
+end
