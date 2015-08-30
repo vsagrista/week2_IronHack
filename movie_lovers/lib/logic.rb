f86eea938f902386fe8961a7436f13974849df88
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-30 20:10:22 +0200

    Added a new class for questions

--- movie_lovers/lib/logic.rb
+++ movie_lovers/lib/logic.rb
@@ -3,7 +3,7 @@ require "imdb"
 require "pry"
 
 class Movie
-attr_accessor :movies
+attr_accessor :movies 
 	def initialize
 		@movies = []
 	end
@@ -11,10 +11,24 @@ attr_accessor :movies
 		list = Imdb::Search.new(keyword)
 		list.movies.each do |movie|
 			if movie.poster != nil
-       		 @movies << movie
-      		end
-        	break if @movies.length == 9
-      	end
+        @movies << movie
+      end
+        break if @movies.length == 9
+      end
 		@movies
+	end
+end
+
+class Question 
+attr_accessor :question
+	def get_random_question(array,sample_number)
+		if sample_number.odd?
+			sample_date = array[sample_number].year
+			@question = "Which movie was filmed in #{sample_date}?"
+		else 
+			sample_writer = array[sample_number].writers
+			@question = "Which movie was directed by #{sample_writer[0]}?"
+		end
+		@question
 	end
 end
