b08b8540cde991a607a01901a5db991763693027
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-30 11:43:34 +0200

    Added question about displayed movies

--- movie_lovers/movieloversapp.rb
+++ movie_lovers/movieloversapp.rb
@@ -4,13 +4,18 @@ require "sinatra/reloader"
 require "pry"
 require_relative "./lib/logic.rb"
 
+movie_search = Movie.new
+random_array = [0,1,2,3,4,5,6,7,8]
 
 get "/" do 
+	@question = "Let's see how much you know about movies..."
 	erb :index
 end
 
 post "/search_keyword" do 
 	keyword = params[:keyword]
-	@movies = Movie.new.get_movies_by_keyword(keyword)
+	@movies =movie_search.get_movies_by_keyword(keyword)
+	@sample_date = @movies[random_array.sample].year
+	@question = "Which movie was filmed in #{@sample_date}?"
 	erb :index
 end
