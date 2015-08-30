a967f92f1a4da5afb0aa8030da51942e73162be3
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-08-30 13:33:24 +0200

    Made program ask random questions

--- movie_lovers/movieloversapp.rb
+++ movie_lovers/movieloversapp.rb
@@ -3,9 +3,10 @@ require "sinatra"
 require "sinatra/reloader"
 require "pry"
 require_relative "./lib/logic.rb"
+enable :sessions
 
-movie_search = Movie.new
-random_array = [0,1,2,3,4,5,6,7,8]
+keyword = ""
+random_array = [0,1,2]
 
 get "/" do 
 	@question = "Let's see how much you know about movies..."
@@ -14,8 +15,22 @@ end
 
 post "/search_keyword" do 
 	keyword = params[:keyword]
-	@movies =movie_search.get_movies_by_keyword(keyword)
-	@sample_date = @movies[random_array.sample].year
+	@movies = Movie.new.get_movies_by_keyword(keyword)
+	random_array_sample = random_array.sample
+	random_array.delete(random_array_sample)
+	@sample_date = @movies[random_array_sample].year
 	@question = "Which movie was filmed in #{@sample_date}?"
-	erb :index
+	erb :search
+end
+
+get "/correct_answer" do 
+	@movies = Movie.new.get_movies_by_keyword(keyword)
+	random_array_sample = random_array.sample
+	@sample_date = @movies[random_array_sample].year
+	@question = "Which movie was filmed in #{@sample_date}?"
+	random_array.delete(random_array_sample)
+	if random_array.length == 0
+		@question = "Wow! You answered them all right, congrats!"
+	end
+	erb :search
 end
